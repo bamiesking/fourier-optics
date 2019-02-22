b1478aabe049e09e8ecf5e77efd4b723235ca4b1
bamiesking <bamiesking@gmail.com>
2019-02-22 18:31:50 +0000

    Added outline of code for 4f emulator class
    Code included is intended to be a template and is untested.

---
+++ 4f_emulator.py
@@ -0,0 +1,17 @@
+import numpy
+
+
+class Emulator():
+    # Class takes discrete 2D input field i (nxn numpy array), and 2D aperture function in Fourier plane f
+    def __init__(self, i, f):
+        self.input_field = i
+        self.fourier_aperture = f
+
+    # Simulates transmission of light through a lens, returning field in rear focal plane
+    def lens_transmission(input):
+        return numpy.fft.fft2(input)
+
+    # Determine field incident on Fourier plane
+    self.fourier_field = lens_transmission(self.input_field)
+    self.fourier_transmission = self.fourier_field * self.fourier_aperture
+    self.output_field = lens_transmission(self.fourier_transmission)
