413d3edc729ec9cd70848f573f5805c733c167bc
bamiesking <bamiesking@gmail.com>
2019-02-27 18:42:40 +0000

    Tidied up emulator.py
    Removed clumsy variable names and changed scope of all variables apart from images in each plane.

--- emulator.py
+++ emulator.py
@@ -7,37 +7,30 @@ class Emulator():
 
     Produces PIL images representing field in Fourier plane and in output plane."""
     def __init__(self, i, f):
-        self.input_image = Image.open(i).convert(mode='L')
-        self.fourier_aperture = f
+        input_image = Image.open(i).convert(mode='L')
+        fourier_aperture = f
 
         # Determine field incident on Fourier plane
-        self.fourier_field = np.fft.fft2(self.input_image, norm='ortho')
-        self.fourier_transmission = self.fourier_field * self.fourier_aperture
-        self.output_field = np.fft.fftn(self.fourier_transmission, norm='ortho')
-        self.output_image = Image.fromarray(np.abs(self.output_field))
+        fourier_field = np.fft.fft2(input_image, norm='ortho')
+        fourier_transmission = fourier_field * fourier_aperture
+        output_field = np.fft.fftn(fourier_transmission, norm='ortho')
+        output_image = Image.fromarray(np.abs(output_field))
 
         #  Transform Fourier plane image to represent proper field
-        fourier_image_before = Image.fromarray(np.abs(self.fourier_transmission))
-        quarters = [fourier_image_before.crop(
-            (0, 0, int(fourier_image_before.width / 2), int(fourier_image_before.height / 2))),
-                    fourier_image_before.crop((int(fourier_image_before.width / 2), 0, int(fourier_image_before.width),
-                                               int(fourier_image_before.height / 2))),
-                    fourier_image_before.crop((0, int(fourier_image_before.height / 2),
-                                               int(fourier_image_before.width / 2), int(fourier_image_before.height))),
-                    fourier_image_before.crop((
-                                              int(fourier_image_before.width / 2), int(fourier_image_before.height / 2),
-                                              int(fourier_image_before.width), int(fourier_image_before.height)))]
+        fourier_image = Image.fromarray(np.abs(fourier_transmission))
+        w, h = fourier_image.size
+        quarters = [fourier_image.crop((0, 0, int(w/2), int(h/2))),
+                    fourier_image.crop((int(w/2), 0, int(w),int(h/2))),
+                    fourier_image.crop((0, int(h/2),int(w/2), int(h))),
+                    fourier_image.crop((int(w/2), int(h/2), int(w), int(h)))]
 
         for i in range(0, 4):
             quarters[i] = quarters[i].rotate(180)
 
-        fourier_image_before.paste(quarters[0],
-                                   (0, 0, int(fourier_image_before.width / 2), int(fourier_image_before.height / 2)))
-        fourier_image_before.paste(quarters[1], (
-        int(fourier_image_before.width / 2), 0, int(fourier_image_before.width), int(fourier_image_before.height / 2)))
-        fourier_image_before.paste(quarters[2], (
-        0, int(fourier_image_before.height / 2), int(fourier_image_before.width / 2), int(fourier_image_before.height)))
-        fourier_image_before.paste(quarters[3], (
-        int(fourier_image_before.width / 2), int(fourier_image_before.height / 2), int(fourier_image_before.width),
-        int(fourier_image_before.height)))
-        self.fourier_image = fourier_image_before
+        fourier_image.paste(quarters[0], (0, 0, int(w/2), int(h/2)))
+        fourier_image.paste(quarters[1], (int(w/2), 0, int(w), int(h/2)))
+        fourier_image.paste(quarters[2], (0, int(h/2), int(w/2), int(h)))
+        fourier_image.paste(quarters[3], (int(w/2), int(h/2), int(w),int(h)))
+
+        # Create accessible array of images
+        self.images = {'i': input_image, 'f': fourier_image, 'o': output_image}
