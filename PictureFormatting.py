82cf946130c80377374776d4e098af4c042b3508
bamiesking <bamiesking@gmail.com>
2019-02-27 11:55:35 +0000

    Minor edits to PictureFormatting file
    Changed function names to follow PEP8 naming conventions and simplified conditional statements

--- PictureFormatting.py
+++ PictureFormatting.py
@@ -1,23 +1,24 @@
 import numpy
 from PIL import Image
 
-def PicArray(make_array, x, name):
+
+def pic_array(make_array, x, name):
     """This function has two options:
     If the bool make_array is set to True, then this function will output an array from the filename
     of the image, x. In this case, name is a dummy variable.
     If bool is set to False, then this function will output an image from an RGB value array, as given in the variable x. The variable
     name is the filename the image will be saved as"""
-    if make_array = True:
+    if make_array is True:
         array = numpy.asarray(Image.open('x'))
         return array
-    elif make_array = False:
+    else:
         im = Image.fromarray(numpy.uint8(x))
         im.save(name)
         im.show()
 
 
-def takePic(filename):
-    image = Image.open(filename)
+def take_pic(filename):
+    image = Image.open(filename).convert(mode='L')
     return image
 
 
