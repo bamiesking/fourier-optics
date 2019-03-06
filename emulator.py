61b502f09048c716efec7a0b39ac03b1442b0d70
bamiesking <bamiesking@gmail.com>
2019-03-06 11:02:58 +0000

    Updated Emulator class
    Changed Emulator class to take Fourier aperture in numpy array form rather than a PIL image, and moved code to correct Fourier image axes to a separate function.

--- emulator.py
+++ emulator.py
@@ -8,29 +8,35 @@ class Emulator():
     Produces PIL images representing field in Fourier plane and in output plane."""
     def __init__(self, i, f):
         input_image = Image.open(i).convert(mode='L')
-        fourier_aperture = f
+        fourier_aperture = np.array(rotate_quarters(Image.fromarray(f)))
 
         # Determine field incident on Fourier plane
         fourier_field = np.fft.fft2(input_image, norm='ortho')
+
         fourier_transmission = fourier_field * fourier_aperture
         output_field = np.fft.fftn(fourier_transmission, norm='ortho')
         output_image = Image.fromarray(np.abs(output_field))
 
         #  Transform Fourier plane image to represent proper field
         fourier_image = Image.fromarray(np.abs(fourier_transmission))
-        w, h = fourier_image.size
-        quarters = [fourier_image.crop((0, 0, int(w/2), int(h/2))),
-                    fourier_image.crop((int(w/2), 0, int(w),int(h/2))),
-                    fourier_image.crop((0, int(h/2),int(w/2), int(h))),
-                    fourier_image.crop((int(w/2), int(h/2), int(w), int(h)))]
-
-        for i in range(0, 4):
-            quarters[i] = quarters[i].rotate(180)
-
-        fourier_image.paste(quarters[0], (0, 0, int(w/2), int(h/2)))
-        fourier_image.paste(quarters[1], (int(w/2), 0, int(w), int(h/2)))
-        fourier_image.paste(quarters[2], (0, int(h/2), int(w/2), int(h)))
-        fourier_image.paste(quarters[3], (int(w/2), int(h/2), int(w),int(h)))
 
         # Create accessible array of images
         self.images = {'i': input_image, 'f': fourier_image, 'o': output_image}
+
+
+def rotate_quarters(image):
+    """Splits a PIL image into quarters, rotates each quarter by 180 degrees, and recombines them."""
+    w, h = image.size
+    quarters = [image.crop((0, 0, int(w / 2), int(h / 2))),
+                image.crop((int(w / 2), 0, int(w), int(h / 2))),
+                image.crop((0, int(h / 2), int(w / 2), int(h))),
+                image.crop((int(w / 2), int(h / 2), int(w), int(h)))]
+
+    for i in range(0, 4):
+        quarters[i] = quarters[i].rotate(180)
+
+    image.paste(quarters[0], (0, 0, int(w / 2), int(h / 2)))
+    image.paste(quarters[1], (int(w / 2), 0, int(w), int(h / 2)))
+    image.paste(quarters[2], (0, int(h / 2), int(w / 2), int(h)))
+    image.paste(quarters[3], (int(w / 2), int(h / 2), int(w), int(h)))
+    return image
