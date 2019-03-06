ed9381437d265c837b00f499ef183987aae4e3fb
bamiesking <bamiesking@gmail.com>
2019-03-06 11:15:58 +0000

    Removed test code, fixed minor error, and tidied up.

--- filters.py
+++ filters.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 
 
 # Set dimensions of array to be equal to pixels of CCD images
@@ -8,46 +6,42 @@ y, x = 1280, 1024
 
 
 # Define small transparent circular filter
-def transparent_circle(R):
+def transparent_circle(r):
     a = np.zeros((x, y))
-    for i in range(0,x):
-        for j in range(0,y):
-            r = np.sqrt(np.abs(x/2 - i)**2 + np.abs(y/2 - j)**2)
-            if r <= R:
+    for i in range(0, x):
+        for j in range(0, y):
+            R = np.sqrt(np.abs(x/2 - i)**2 + np.abs(y/2 - j)**2)
+            if R <= r:
                 a[i, j] = 1
     print(a.max())
     return a
 
 
 # Define small opaque circular filter as complement of small transparent circular filter
-def opaque_circle(R):
-    return np.full((x, y), 1) - transparent_circle(R)
+def opaque_circle(r):
+    return np.full((x, y), 1) - transparent_circle(r)
 
 
 def grating(d, t):
-    if t %((np.pi)/2) == 0:
+    if t % np.pi/2 == 0:
         b = np.full((x, y), 0.)
         for i in range(0, y, int(y / d)):
-            b[:, i] = np.full((y), 1.0)
+            b[:, i] = np.full(x, 1.0)
         return b
-    elif t%(np.pi) == 0:
+    elif t % np.pi == 0:
         b = np.full((x, y), 0.)
         for i in range(0, x, int(x / d)):
-            b[i,:] = np.ones((x),1.0)
+            b[i, :] = np.ones(y, 1.0)
         return b
-    elif t%((np.pi/2)/2) != 0 and t%(np.pi) != 0:
-        b = np.zeros((x,y))
+    else:
+        b = np.zeros((x, y))
         for i in range(0, x, int((x/d)*np.cos(t))):
-            for j in range(0,y,int((y/d)*np.sin(t))):
-                b[i,j] = 1.0
+            for j in range(0, y, int((y/d)*np.sin(t))):
+                b[i, j] = 1.0
         return b
+
 
 def ones():
-    return np.full((x,y), 1.0)
+    return np.full((x, y), 1.0)
 
-
-if __name__ == '__main__':
-    afilter = transparent_circle(100)
-    plt.imshow(Image.fromarray(afilter*256))
-    plt.show()
 
