37f405cdb312db25c22706f21d16e9391d4bc37f
Unungentium <franciszek.pauwels@durham.ac.uk>
2019-03-09 13:53:29 +0000

    Created a function which successfully plots the intensity values of the real image and the theoretical prediction, showing the accuracy of the measurement

--- Data_analysis.py
+++ Data_analysis.py
@@ -3,23 +3,36 @@ import numpy as np
 from emulator import Emulator
 import filters
 import matplotlib.pyplot as plt
-import scipy
+"""import scipy.optimize
+import scipy.stats
+import scipy.ticker as ticker"""
+
 
 #save two images - im1 is the image taken by the camera, im2 is the image processed by the computer
-e = Emulator("21-02-19/FourDotsInput.tif", filters.ones())
+e = Emulator("21-02-19/FourDotsInput.tif", filters.uniform(1))
 
 im1 = Image.open("21-02-19/FourDotsInput.tif").convert(mode = 'L')
 
 im2 = e.images['o']
+#read data and output it in a useful format
 
-pix1 = im1.load()
-pix2 = im2.load()
-def line_read(i,c, a):
-    """i here is the line to be read and saved as an array
-    c is the length of the line to be read
-    a is the array being read"""
-    b = np.zeros(c)
-    for x in range(c):
-        b[x] = a[i,x]
-    return b
-print (line_read(100, 1024, pix1))
+
+
+def line_read(a):
+    """saves a PIL image as a numpy array, and outputs one line of interest specified by the maximum value of the array.
+    a is the PIL image to be read"""
+    b = np.array(a)
+    d = np.unravel_index(b.argmax(), b.shape)
+
+
+    return b[int(d[0])]
+
+
+
+pix1_arr = line_read(im1)
+pix2_arr = line_read(im2)
+
+plt.plot(pix1_arr, label = 'real image')
+plt.plot(pix2_arr, label = 'theoretical prediction')
+plt.legend()
+plt.show()
